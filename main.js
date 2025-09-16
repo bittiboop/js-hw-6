47204bf805c0773f1be79397d404d1d870dde1cc
bittiboop <sstrelkova15@email.com>
2025-09-16 17:59:42 +0300

    saved the changes

--- main.js
+++ main.js
@@ -44,3 +44,53 @@ while(true){
     }
     console.log("You entered: " + input);
 }
+
+function isPrime(num){
+    if(num <= 1) return false;
+    for(let i = 2; i <= Math.sqrt(num); i++){
+        if(num % i === 0) return false;
+    }
+    return true;
+}
+let number = +prompt("Enter a number to check if it's prime:");
+if(isPrime(number)){
+    console.log(number + " is a prime number.");
+}
+else{
+    console.log(number + " is not a prime number.");
+}
+
+function reverseString(str){
+    return str.split('').reverse().join('');
+}
+console.log("Reversed string: " + reverseString('JavaScript'));
+
+function getFactorial(num){
+    if(num < 0) return -1;
+    if(num === 0) return 1;
+    let fact = 1;
+    for(let i = 1; i <= num; i++){
+        fact *= i;
+    }
+    return fact;
+}
+let factNum = +prompt("Enter a number to get its factorial:");
+console.log("Factorial of " + factNum + " is " + getFactorial(factNum));
+
+
+
+//game "Guess the Number"
+let randomNum = Math.floor(Math.random() * 100) + 1;
+let guess = null;
+while(guess !== randomNum){
+    guess = +prompt("Guess a number between 1 and 100:");
+    if(guess === randomNum){
+        alert("Congratulations! You guessed the number.");
+    }
+    else if(guess < randomNum){
+        alert("Try again! The number is higher.");
+    }
+    else{
+        alert("Try again! The number is lower.");
+    }
+}
